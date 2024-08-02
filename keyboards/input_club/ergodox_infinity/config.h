71c302e72724dce1b273b9eca3eb46fac9c752d2
Joe Bauser <coderjoe@coderjoe.net>
2024-08-01 22:15:27 -0400

    coderjoe ergodox_infinity move reset keys and add boot keys
    Make it esier to flash and reset, and add QK_BOOT to make flashing
    easier. Finally also set up EE_HANDS

--- keyboards/input_club/ergodox_infinity/config.h
+++ keyboards/input_club/ergodox_infinity/config.h
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define EE_HANDS
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    60
